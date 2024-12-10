64d49233a0d0cd447b63466dc431ffc4597ed3e0
neibo <neibo911@gmail.com>
2024-12-10 10:35:22 +0800

    Enable4

--- etw/etw_helpers.go
+++ etw/etw_helpers.go
@@ -565,36 +565,37 @@ func (e *EventRecordHelper) GetPropertyUint(name string) (u uint64, err error) {
 	return strconv.ParseUint(s, 0, 64)
 }
 
-func (e *EventRecordHelper) GetPropertyStack() (stack []uint64, err error) {
-	record := e.EventRec
-	var i uint16
-	err = nil
-
-	if record.ExtendedDataCount != 0 {
-		for i = 0; i < record.ExtendedDataCount; i++ {
-			item := record.ExtendedDataItem(i)
-			if item.ExtType == EVENT_HEADER_EXT_TYPE_STACK_TRACE64 {
-				var j uint16
-
-				stackTrace := (*EventExtendedItemStackTrace64)(unsafe.Pointer(item.DataPtr))
-				stack_length := (item.DataSize - uint16(unsafe.Sizeof(uint64(0)))) / uint16(unsafe.Sizeof(uint64(0)))
-				for j = 0; j < stack_length; j++ {
-					stack = append(stack, stackTrace.Address[j])
-					fmt.Printf("%x", stackTrace.Address[j])
-				}
-			}
-			//if item.ExtType == EVENT_HEADER_EXT_TYPE_STACK_TRACE32 {
-			//	var j uint16
-			//	stackTrace := (*EventExtendedItemStackTrace32)(unsafe.Pointer(item.DataPtr))
-			//	stack_length := (item.DataSize - 8) / uint16(unsafe.Sizeof(uint(0)))
-			//	for j = 0; j < stack_length; j++ {
-			//		stack = append(stack, uint64(stackTrace.Address[j]))
-			//	}
-			//}
-		}
-	}
-	return stack, err
-}
+//func (e *EventRecordHelper) GetPropertyStack() (stack []uint64, err error) {
+//	record := e.EventRec
+//	stack = append(stack, uint64(record.ExtendedDataCount))
+//	//var i uint16
+//	//err = nil
+//	//
+//	//if record.ExtendedDataCount != 0 {
+//	//	for i = 0; i < record.ExtendedDataCount; i++ {
+//	//		item := record.ExtendedDataItem(i)
+//	//		if item.ExtType == EVENT_HEADER_EXT_TYPE_STACK_TRACE64 {
+//	//			var j uint16
+//	//
+//	//			stackTrace := (*EventExtendedItemStackTrace64)(unsafe.Pointer(item.DataPtr))
+//	//			stack_length := (item.DataSize - uint16(unsafe.Sizeof(uint64(0)))) / uint16(unsafe.Sizeof(uint64(0)))
+//	//			for j = 0; j < stack_length; j++ {
+//	//				stack = append(stack, stackTrace.Address[j])
+//	//				fmt.Printf("%x", stackTrace.Address[j])
+//	//			}
+//	//		}
+//	//		//if item.ExtType == EVENT_HEADER_EXT_TYPE_STACK_TRACE32 {
+//	//		//	var j uint16
+//	//		//	stackTrace := (*EventExtendedItemStackTrace32)(unsafe.Pointer(item.DataPtr))
+//	//		//	stack_length := (item.DataSize - 8) / uint16(unsafe.Sizeof(uint(0)))
+//	//		//	for j = 0; j < stack_length; j++ {
+//	//		//		stack = append(stack, uint64(stackTrace.Address[j]))
+//	//		//	}
+//	//		//}
+//	//	}
+//	//}
+//	return
+//}
 
 func (e *EventRecordHelper) SetProperty(name, value string) {
 
